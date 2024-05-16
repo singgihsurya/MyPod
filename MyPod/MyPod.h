9406ccc0c962772ee18c0af47aae57917df79e2b
singgih dharma <singgih.dharma@mncbank.co.id>
2024-05-16 08:13:15 +0700

    Initial project setup

---
+++ MyPod/MyPod.h
@@ -0,0 +1,18 @@
+//
+//  MyPod.h
+//  MyPod
+//
+//  Created by Macbook on 16/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPod.
+FOUNDATION_EXPORT double MyPodVersionNumber;
+
+//! Project version string for MyPod.
+FOUNDATION_EXPORT const unsigned char MyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPod/PublicHeader.h>
+
+
---
+++ MyPodExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPodExample
+//
+//  Created by Macbook on 16/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
