a3b1a803c4f1c470a424bfbbf92ca62947c70fc7
singgih dharma <singgih.dharma@mncbank.co.id>
2024-05-16 09:54:16 +0700

    Added function

---
+++ MyPod/MyPod.swift
@@ -0,0 +1,22 @@
+//
+//  MyPod.swift
+//  MyPod
+//
+//  Created by Macbook on 16/05/24.
+//
+
+import Foundation
+
+public final class MyPod {
+
+    let name = "MyPod"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
